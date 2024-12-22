93d59c3914cfade5c76b4131cc327150c893f9aa
Samir Khanal <gdssamir@gmail.com>
2024-12-22 17:39:20 +0545

    featured

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Featured from "@/components/Featured";
 import HeroSection from "@/components/HeroSection";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <HeroSection />
+      <Featured />
     </main>
   );
 }
